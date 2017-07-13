af14f48e82320aa77c6bcb9a5d5fa66d05bf0585
Stefan Tannhaeuser <stefan.tannhaeuser@ericsson.com>
2017-07-13 23:52:10 +0200

    Resourcegroup #5

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get -y update
 
+dbpass=$1
+
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
