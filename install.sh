57c186fa0da33552c5f5d277fe6b4cba6cd56f61
Stefan Tannhaeuser <stefan.tannhaeuser@ericsson.com>
2017-07-13 23:38:22 +0200

    Raw Linux VM #2

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+apt-get -y update
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" typo3
+
+apachectl restart
